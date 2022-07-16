84e159e9f02fceaa9e43f46b78990e927f81098c
Nikita <bigmama93@mail.ru>
2022-07-16 22:04:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import telegram
+from environs import Env
+from urllib.parse import urljoin
+
+
+def main():
+    env = Env()
+    env.read_env()
+    chat_id = env('TG_CHAT_ID')
+    tg_token = env('TG_TOKEN')
+    bot = telegram.Bot(tg_token)
+    current_price = 9350
+    while True:
+        url = 'https://sotohit.ru/internet-magazin2/folder/apple-airpods'
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        title_airpods = soup.find_all(class_='uk-grid-item shop2-product-item')
+        for i in title_airpods:
+            title = i.find(class_='product-item-name')
+            if 'Apple AirPods 2 ' in title.text:
+                link = title.find('a').get('href')
+                airpods2_url = urljoin('https://sotohit.ru/', link)
+                response_product = requests.get(airpods2_url)
+                response_product.raise_for_status()
+                airpods2_soup = BeautifulSoup(response_product.text, 'lxml')
+                airpods2_title = airpods2_soup.find('h1').text
+                airpods2_price = airpods2_soup.find(class_='price-current').find('strong').text
+                if int(airpods2_price) != current_price:
+                    bot.send_message(text=f'цена {airpods2_title} теперь такая {airpods2_price} \n заказать {airpods2_url}',
+                                     chat_id=chat_id)
+        time.sleep(3600)
+
+
+if __name__ == '__main__':
+    main()
